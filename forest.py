ca978892e4a0dad6cf182fce7880d15e769d0bdb
Nikolaus Vertovec <niko.vertovec@outlook.com>
2022-11-02 18:41:21 +0000

    first working version of generating a hypothesis

--- forest.py
+++ forest.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from mip import Model, minimize, xsum, BINARY, CONTINUOUS
 class Forest:
     # Parameters for propagation
     wind_direction = 2*pi * 0
@@ -12,23 +13,27 @@ class Forest:
     p = np.clip(base_prop + wind_strength * p_rotation, 0, 1) # Propagation prob
     
     # Colours for potting
-    fire_colour = [0.87,0.3,0.2,0.9]
-    tree_colour = [0.156,0.59,0,1]
-    ground_colour = [0.29,0.01,0,0.6]
-    burnt_colour = [0.1,0.1,0.1,1]
+    fire_colour = [0.87,0.3,0.2,0.9]    # forest == 2
+    tree_colour = [0.156,0.59,0,1]      # forest == 3
+    ground_colour = [0.29,0.01,0,0.6]   # forest == 0
+    burnt_colour = [0.1,0.1,0.1,1]      # forest == 1
     CM = mpl.colors.ListedColormap([ground_colour,burnt_colour,fire_colour, tree_colour])
     
 
-    def __init__(self, l,h,d):
+    def __init__(self,w,d):
         # Create a simple forest
-        #    l - length
-        #    h - height
+        #    w - width for a square forest
         #    d - forest denisty
-        self.forest = np.zeros((l,h))
-        for y in range(h):
-            for x in range(l):
+        self.forest = np.zeros((w,w))
+        self.forest_width = w
+        for y in range(w):
+            for x in range(w):
                 self.forest[y,x] = 3*(random.random() <= d)
         self.im = plt.imshow(self.forest, cmap=self.CM, interpolation='none')
+        plt.show(block=False)
+
+    def update_plt(self):
+        self.im.set_array(self.forest)
         plt.show(block=False)
 
     def basic_fire_prop(self):
@@ -56,6 +61,95 @@ class Forest:
         for i in range(len(burnt_row)):
             self.forest[burnt_row[i]][burnt_col[i]] = 1
         self.forest = np.clip(self.forest,0,3)
+
+    def genSamples(self, m=1):
+        # Assuming forest.shape[0] = forest.shape[1]
+        x = np.random.randint(1, high=self.forest_width, size=(m,2))
+        f = np.zeros((m,1))
+        for i in range(m):
+            self.basic_fire_prop()
+            # Burnt or on fire land
+            f[i] = (self.forest[x[i, 0], x[i,1]] == 2 or self.forest[x[i, 0], x[i,1]] == 1)
+        return (x, f)
+
+
+    def generateMsampleModel(self, nr_samples):
+        # Get samples
+        (x,f) = self.genSamples(m=nr_samples)
+        m = len(f)
+
+        # Number of facets
+        Nf_hf = len(x[0])
+        Nf = 2*Nf_hf
+        
+        a = np.identity(Nf_hf) # aj = a[j]
+        a = np.block([[a], [-a]])
+        # Assuming Mj = -mj = self.forest_width (upperbound/lowerbound for x[i,j])
+        M = self.forest_width
+        eps = 0.0001
+
+        model = Model()
+        v = {i: model.add_var(var_type=BINARY, name="v[%d ]" % i) for i in range(m)}
+        b = {j: model.add_var(var_type=CONTINUOUS, lb = -M, ub = M, name="b[%d]" % j) for j in range(Nf)}
+        z = {(i,j): model.add_var(var_type=BINARY, name="z[%d, %d]" % (i, j)) for i in range(m) for j in range(Nf)}
+        s = {(i,j): model.add_var(var_type=CONTINUOUS, lb = 0, ub = M, name="s[%d, %d]" % (i, j)) for i in range(m) for j in range(Nf)}
+        
+        model.objective = minimize(xsum(v[i] for i in range(m)))
+
+        for i in range(m):
+            if f[i]: # constraints for i in I1
+                for j in range(Nf):
+                    model.add_constr(np.matmul(a[j], x[i]) + b[j] - s[i,j] <= 0)
+                    model.add_constr(xsum(s[i, j] for j in range(Nf)) - v[i] * Nf * M <= 0)
+            else:# constraints for i in I0
+                for j in range(Nf):
+                    model.add_constr(np.matmul(a[j], x[i]) + b[j] - M*(1 - z[i,j]) <= 0)
+                    model.add_constr(eps + (-M - eps)*z[i,j] - s[i,j] - np.matmul(a[j], x[i])- b[j]<= 0)
+                    model.add_constr(xsum(z[i, j] for j in range(Nf)) + 1 - Nf <= 0)
+                    model.add_constr(xsum(s[i, j] for j in range(Nf)) - v[i] * Nf * M <= 0)
+        width = {j: model.add_var(var_type=CONTINUOUS, lb = -M, ub = M, name="width[%d]" % j) for j in range(Nf_hf)}
+        for j in range(Nf_hf):
+            model.add_constr(-b[j] - b[j + Nf_hf] == width[j])
+            model.add_constr(width[j] >= 0)
+
+    # ###### Test Model ########
+    #     x_true = np.empty((int(sum(f)), 2))
+    #     iter_true = 0
+    #     model_true = Model()
+    #     b = {j: model_true.add_var(var_type=CONTINUOUS, lb = -M, ub = M, name="b[%d]" % j) for j in range(Nf)}
+    #     for i in range(m):
+    #         if f[i]: # constraints for i in I1
+    #             x_true[iter_true] = x[i]
+    #             iter_true += 1
+    #             for j in range(Nf):
+    #                 model_true.add_constr(np.matmul(a[j], x[i]) + b[j] <= 0)
+
+    #     width = {j: model_true.add_var(var_type=CONTINUOUS, lb = -M, ub = M, name="width[%d]" % j) for j in range(Nf_hf)}
+    #     for j in range(Nf_hf):
+    #         model_true.add_constr(-b[j] - b[j + Nf_hf] == width[j])
+    #         model_true.add_constr(width[j] >= 0)
+    #     model_true.objective = minimize(xsum(width[j] for j in range(Nf_hf)))
+
+
+    #     b_check = [-max(x_true[:,0]), -max(x_true[:,1]), min(x_true[:,0]), min(x_true[:,1])]
+    #     for example in x_true:
+    #         for j in range(Nf):
+    #             if np.matmul(a[j], example) + b_check[j] > 0:
+    #                 print('found error')
+    #     model = model_true
+    # #######
+        
+        model.x = x
+        model.f = f
+
+        model.v = v
+        model.a = a
+        model.b = b
+        model.z = z
+        model.s = s
+        model.width = width
+
+        return model
 
     def updatefig(self, *args):
         self.basic_fire_prop()
--- forest_fire.py
+++ forest_fire.py
@@ -1,22 +1,55 @@
 from math import*
 import numpy as np
 import random
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import animation
-import matplotlib as mpl
+from matplotlib.patches import Rectangle
 from forest import Forest
+from mip import Model, OptimizationStatus
 
-
-fig = plt.figure(figsize=(5,5))
-my_forest = Forest(400, 400, 0.6)
+fig1 = plt.figure(figsize=(5,5))
+my_forest = Forest(100, 0.6)
 
 # initilaise a fire
-my_forest.forest[200, 150] = 2
+my_forest.forest[50, 60] = 2
 plt.pause(0.001)
 
-anim = animation.FuncAnimation(fig, my_forest.updatefig, interval=10, blit=True)
-anim.save('animation.mp4', fps=20, writer="ffmpeg", codec="libx264")
-plt.axis('off')
+sample_count = 400
+model = my_forest.generateMsampleModel(sample_count)
+my_forest.update_plt()
+status = model.optimize()
+if status == OptimizationStatus.OPTIMAL:
+    print('optimal solution cost {} found'.format(model.objective_value))
+elif status == OptimizationStatus.FEASIBLE:
+    print('sol.cost {} found, best possible: {} '.format(model.objective_value, model.objective_bound))
+elif status == OptimizationStatus.NO_SOLUTION_FOUND:
+    print('no feasible solution found, lower bound is: {} '.format(model.objective_bound))
+
+fig2 = plt.figure(figsize=(5,5))
+sample_array = np.zeros(my_forest.forest.shape)
+for i in range(sample_count):
+    sample_array[model.x[i][0], model.x[i][1]] = 1 + int(model.f[i])
+
+ # Colours for potting
+two_colour = [0.156, 1,0,1]  
+one_colour = [1,0.01,0,0.6] 
+zero_colour = [1 ,1 , 1 ,1]  
+CM = mpl.colors.ListedColormap([zero_colour,one_colour,two_colour])
+plt.imshow(sample_array, cmap=CM, interpolation='none')
+plt.show(block=False)
+
+Nf_hf = 2
+y_min = min(abs(model.b[0].x), abs(model.b[2].x))
+x_min = min(abs(model.b[1].x), abs(model.b[3].x))
+plt.gca().add_patch(Rectangle((x_min,y_min),model.width[1],model.width[0],
+                    edgecolor='blue',
+                    facecolor='none',
+                    lw=1))
+
+# anim = animation.FuncAnimation(fig1, my_forest.updatefig, interval=10, blit=True)
+# anim.save('animation.mp4', fps=20, writer="ffmpeg", codec="libx264")
+# plt.axis('off')
 plt.show()
 
 
